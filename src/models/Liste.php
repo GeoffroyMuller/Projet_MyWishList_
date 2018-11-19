86a1231f3743863a1681037f88bfe1cec579d80d
lucasguezennec <lucas.guezennec2@etu.univ-lorraine.fr>
2018-11-19 15:50:51 +0100

    Ajout de Liste.php

--- src/models/Liste.php
+++ src/models/Liste.php
@@ -3,17 +3,16 @@
  * Created by PhpStorm.
  * User: Lucas
  * Date: 19/11/2018
- * Time: 15:35
+ * Time: 15:49
  */
 
 namespace mywishlist\models;
 
 
-class Item extends \Illuminate\Database\Eloquent\Model
+class Liste extends \Illuminate\Database\Eloquent\Model
 {
-    protected $table = 'item';
-    protected $primaryKey = 'id';
+    protected $table = 'list';
+    protected $primaryKey = 'no';
     public $timestamps = false;
-
 
 }
