94d49318db3ac7f246d7f211fb2017fb59454e8e
lucasguezennec <lucas.guezennec2@etu.univ-lorraine.fr>
2018-11-19 15:47:46 +0100

    Ajout de Item.php

---
+++ src/models/Item.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucas
+ * Date: 19/11/2018
+ * Time: 15:35
+ */
+
+namespace mywishlist\models;
+
+
+class Item extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 'item';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    
+}
