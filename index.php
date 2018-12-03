0f71da9d972b676336abf4a5e08ae2f96b3a652f
lucasguezennec <lucas.guezennec2@etu.univ-lorraine.fr>
2018-12-03 15:44:25 +0100

    Correction de bug afficherToutesLesListes

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
 require_once 'vendor/autoload.php';
+use Illuminate\Database\Capsule\Manager as DB;
 
+$db = new DB();
+$db->addConnection(parse_ini_file('./src/conf/conf.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
 $app = new \Slim\Slim ;
 
-echo 'Test';
 
-$app->get('/affichage/afficherTouteLesListes',function (){
-    //$controlleurAffichage = new \mywishlist\controlleurs\Affichage();
-    echo 'SALUT';
-    //echo $controlleurAffichage->afficherToutLesItems();
+$app->get('/affichage/afficherToutesLesListes',function (){
+    $controlleurAffichage = new \mywishlist\controlleurs\Affichage();
+    echo $controlleurAffichage->afficherToutLesItems();
 });
 
 $app->run();
