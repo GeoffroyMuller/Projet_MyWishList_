a260a4c309a04679770c649ea8f8aa45937e7370
Geoffroy Muller <johndoe@example.comgeoffroym2@gmail.com>
2018-12-03 15:29:54 +0100

    ajout affichlistesouhait

--- index.php
+++ index.php
@@ -6,3 +6,10 @@ $app = new \Slim\Slim ;
 $app->get('/affichage/afficherTouteLesListes',function (){
 
 });
+
+$app->get('/affichage/afficherListeSouhait', function (){
+    echo 'afficherListeSouhait';
+});
+
+$app->run();
+
