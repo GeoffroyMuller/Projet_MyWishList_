f3a72020bd6f1d70c0e1e358e7cf384b74de5bb6
lucasguezennec <lucas.guezennec2@etu.univ-lorraine.fr>
2018-12-10 15:31:46 +0100

    Modification dans Affichage.php

--- src/controlleurs/Affichage.php
+++ src/controlleurs/Affichage.php
@@ -9,32 +9,34 @@
 namespace mywishlist\controlleurs;
 
 
+use mywishlist\vue\VueParticipant;
+
 class Affichage
 {
+    /**
+     * Méthode affichant un item
+     * @param $id
+     *      Id de l'item
+     * @return string
+     *      Code html de la vue
+     */
     public function afficherItem($id){
 
         $res = \mywishlist\models\Item::select('id', 'nom', 'descr')
             ->where('id', '=', $id)->get();
-        return $res;
+        $vue = new VueParticipant($res,"ITEM");
+        return $vue->render();
     }
 
 
-    public function afficherToutLesItems(){
-        $resultat="";
-        $listes_de_souhaits = \mywishlist\models\Liste::select('user_id','titre','description','expiration')->get();
-        foreach ($listes_de_souhaits as $liste){
-            $resultat=$resultat.$liste->no . ':'.$liste->user_id . ':' . $liste->titre . ':' . $liste->description . ':' . $liste->expiration."<br>";
-        }
-        return $resultat;
-    }
     public function afficherListeItems($idlisteSouhait){
         $resultat = array();
         $liste_de_souhait = \mywishlist\models\Liste::where('no', '=', $idlisteSouhait)->first();
         $ListeItems = $liste_de_souhait->items()->get();
         foreach ($ListeItems as $item){
-            //$resultat=$resultat.$Item->id.":".$Item->liste_id.":".$Item->nom.":".$Item->descr.":".$Item->img.":".$Item->url.":".$Item->tarif."<br>";
             array_push($resultat, $this->afficherItem($item->no));
         }
-        return $resultat;
+        $vue = new VueParticipant($resultat,"ITEM");
+        return $vue->render();
     }
 }
