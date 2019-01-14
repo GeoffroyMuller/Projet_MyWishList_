28395812d3a29102780d5770494522e68217b689
joyscraft <justin.plaid8@etu.univ-lorraine.fr>
2019-01-14 12:54:32 +0100

    commentaire

--- index.php
+++ index.php
@@ -69,14 +69,14 @@ $app->get('/modifierItem/:id',function($id){
 /**
  * Url permettant d'ajouter un commentaire
  */
-$app->post('/createur/ajoutCommentaire/:id',function($id){
+$app->post('/createur/ajoutCommentaire/:id',function($id,$no){
     $controlleurCreateur = new \mywishlist\models\Commentaire();
     //Vérification des données entrée par l'utilisateur
     $message="";
     if(isset($_POST['message'])){
         $controleur = new mywishlist\controlleurs\Createur();
         $message =filter_var($_POST['message'],FILTER_SANITIZE_STRING);
-        $controleur->ajouterMessage($id,,$message);
+        $controleur->ajouterMessage($id,$no,$message);
     }
     $app = \Slim\Slim::getInstance();
     $app->redirect($app->urlFor("afficherItemsListe",['id'=>$id]));
